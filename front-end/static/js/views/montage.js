e72a34f58af47536ee19fa5b2d618b69739e8ef9
kev-ye <kangkai.ye@hotmail.com>
2022-04-26 19:19:36 +0200

    save: montage progressing

--- front-end/static/js/views/montage.js
+++ front-end/static/js/views/montage.js
@@ -62,21 +62,47 @@ export default class extends AbstractView {
 
   createNewThumbnails(src, imageInfo) {
     const imageCollect = document.getElementById('image-collect');
+
+    if (Array.from(imageCollect.childNodes).length >= 9) {
+      alert('You can only take 9 photos on same time!');
+      return ;
+    }
+
     const imageList = document.createElement('li');
     const imageCanvas = document.createElement('canvas');
+    const imageDelete = document.createElement('span');
 
-    imageList.id = `image-${imageInfo.imageIdx++}`;
+    imageList.id = `image-${imageInfo.imageIdx}`;
     imageList.classList.add('image-collect-thumbnail-container');
 
     imageCanvas.classList.add('image-collect-thumbnail');
 
+    imageDelete.id = `image-delete-${imageInfo.imageIdx}`;
+    imageDelete.classList.add('image-delete', 'material-icons');
+
+    imageDelete.innerHTML = 'close';
+
     imageCollect.appendChild(imageList);
+    imageList.appendChild(imageDelete);
     imageList.appendChild(imageCanvas);
 
     imageCanvas.width = imageInfo.width;
     imageCanvas.height = imageInfo.height;
     imageCanvas.getContext('2d').drawImage(src, 0, 0);
 
-    console.log('id:', imageList.id);
+    imageDelete.onclick = () => {
+      imageCollect.removeChild(imageList);
+    }
+
+    imageInfo.imageIdx++;
+
+    // console.log(imageCanvas.toDataURL('image/jpeg', 1.0));
+  }
+
+  async uploadThumbnails() {
+    const imageCollect = document.getElementById('image-collect');
+
+    // need create a request in back,
+    // user readSyncFile to create a image file by dataUrl
   }
 }
